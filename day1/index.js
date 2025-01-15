cdd747487d9cb382ec73dee7e9e4197ef3be81b4
Nisha <rainishani400@gmail.com>
2025-01-15 08:29:44 +0545

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+//mongoose.connect('mongodb://127.0.0.1:27017/test');
+main()
+.then(()=>{
+    console.log("connection ssucessful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+const userSchema = new mongoose.Schema({
+    name:String,
+    email: String,
+    age:Number,
+});
