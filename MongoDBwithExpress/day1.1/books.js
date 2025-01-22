f26359b508acd17f86df1b44218d630a28774bcd
Nisha <rainishani400@gmail.com>
2025-01-22 21:39:31 +0545

    Add files via upload

---
+++ MongoDBwithExpress/day1.1/books.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+//mongoose.connect('mongodb://127.0.0.1:27017/test');
+main()
+.then(()=>{
+    console.log("connection ssucessful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+const bookSchema = new mongoose.Schema({
+     title :{
+        type : String,
+        required : true,
+     },
+     author :{
+        type :String,
+     },
+     price :{
+        type: Number,
+     },
+     discount :{
+        type :Number,
+        default: 0,
+     },
+});
+const Book = mongoose.model("Book",bookSchema);
+let book1 = new Book ({
+title : "Gone Girl",
+//author : "Dr.sabita",
+price :3000
+})
+book1.save().then(res =>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.log(err);
+});
