1301506f6a25c4e9c7139d15e22a30f9338933af
Nisha <rainishani400@gmail.com>
2025-01-24 22:01:17 +0545

    Add files via upload

---
+++ MongoDBwithExpress/day1.2/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+// getting-started.js
+const mongoose = require('mongoose');
+const path = require("path");
+const Chat = require('./models/chat.js');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine","ejs");
+
+main()
+.then(()=>{
+    console.log("connection successful");
+})
+.catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/Whatsapp');
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+let chat1 = new Chat ({
+    from :"nisha",
+    to: "saraj",
+    msg:"best of luck",
+    created_at : new Date(),
+
+});
+chat1.save().then((res)=>{
+console.log(res);
+});
+
+
+
+app.get("/", (req, res) => {
+    res.send(" root is working ");
+});
+app.listen(4040, () => {
+    console.log("server is listening on port 4040");
+});
